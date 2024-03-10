8fb8c2f6c58c6e8f7823b485f216de3692328724
Jaime <jaimenovillobenito@gmail.com>
2024-03-10 14:11:19 +0100

    checkbox consentimiento

--- scripts/form.js
+++ scripts/form.js
@@ -3,13 +3,12 @@ document.querySelector('form').addEventListener('submit', function(event) {
 
     var nombre = document.getElementById('nombre').value;
     var apellidos = document.getElementById('apellidos').value;
-    var correo = document.getElementById('correo').value;
     var empresa = document.getElementById('empresa').value;
     var url = document.getElementById('url').value;
     var asunto = document.getElementById('asunto').value;
     var descripcion = document.getElementById('descripcion').value;
 
-    var cuerpoCorreo = `Nombre: ${nombre}\nApellidos: ${apellidos}\nCorreo: ${correo}\nEmpresa: ${empresa}\nURL de la web: ${url}\nDescripción: ${descripcion}`;
+    var cuerpoCorreo = `Nombre: ${nombre}\nApellidos: ${apellidos}\nEmpresa: ${empresa}\nURL de la web: ${url}\nDescripción: ${descripcion}`;
 
     window.location.href = `mailto:pruebajesus@proton.me?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpoCorreo)}`;
 });
