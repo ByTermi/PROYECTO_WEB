1f436ec24b1a23d70408bcae337b16137920891d
Jaime <jaimenovillobenito@gmail.com>
2024-03-10 18:25:29 +0100

    rutas, css y arch solucionados

--- scripts/arch.js
+++ scripts/arch.js
@@ -1,12 +1,16 @@
 let fondo = document.getElementById('fondoPdf').style;
 let objeto = document.getElementById('objectPdf').style;
+let documento = document.getElementsByTagName('body');
 function abrirArchivo(){
     
     if(fondo.visibility=='visible'||objeto.visibility=='visible'){
         fondo.visibility='hidden';
         objeto.visibility='hidden';
+        document.documentElement.style.overflow = 'auto';
     }else{
         fondo.visibility='visible';
         objeto.visibility='visible';
+        window.scrollTo(0, 0);
+        document.documentElement.style.overflow = 'hidden';
     }
 }
